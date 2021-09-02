c8def83f255f0f8d56946285a72dd6909b27f42b
Matthewshvorin <71457948+mshvorin@users.noreply.github.com>
2021-09-02 04:19:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+
+win=turtle.Screen()
+crush=turtle.Turtle()
+crush.color("red")
+crush.forward(50)
+crush.left(90)
+crush.forward(50)
+crush.left(90)
+crush.forward(50)
+crush.left(90)
+crush.forward(50)
+crush.left(90)
+
+turtle2=turtle.Turtle()
+turtle2.color("green")
+turtle2.forward(60)
+turtle2.left(120)
+turtle2.forward(60)
+turtle2.left(120)
+turtle2.forward(60)
+turtle2.left(120)
