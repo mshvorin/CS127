98fa745cebe7e059dfdc2065bdb9b22e22b30c1c
Matthew Shvorin <71457948+mshvorin@users.noreply.github.com>
2021-09-08 16:56:05 +0000

    used repetition to create 3 shapes of various colors and sizes in fewer lines of code.

--- shapes/main.py
+++ shapes/main.py
@@ -1,6 +1,7 @@
 import turtle
-
 win=turtle.Screen()
+
+#square
 crush=turtle.Turtle()
 crush.color("red")
 crush.forward(50)
@@ -11,14 +12,18 @@ crush.forward(50)
 crush.left(90)
 crush.forward(50)
 crush.left(90)
-pen.penup()
-pen.goto(-50,-50)
-pen.pendown()
+#triangle
 turtle2=turtle.Turtle()
 turtle2.color("green")
+turtle2.penup()
+turtle2.goto(-50,-50)
+turtle2.pendown()
 turtle2.forward(60)
 turtle2.left(120)
 turtle2.forward(60)
 turtle2.left(120)
 turtle2.forward(60)
 turtle2.left(120)
+
+win.exitonclick()
+win.mainloop()
